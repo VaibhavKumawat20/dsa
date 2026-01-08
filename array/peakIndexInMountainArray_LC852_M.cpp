605f1bc5ccb9c4a7bd05cbac4c7cc20629cdec8a
VaibhavKumawat20 <vaibhav20kumawat@gmail.com>
2026-01-08 10:02:54 +0530

    add

---
+++ array/peakIndexInMountainArray_LC852_M.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int peakIndexInMountainArray(vector<int>& arr) {
+        int st = 1, end = arr.size()-2;
+        
+        while(st <= end){
+            int mid = st + (end-st)/2;
+            if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1])
+                return mid;
+            if(arr[mid] > arr[mid-1])
+                st = mid+1;
+            else
+                end = mid-1;
+        }
+        return -1;
+    }
+
+    int main(){
+        vector<int> arr = {3, 4, 5, 10, 0};
+        int index = peakIndexInMountainArray(arr);
+        cout << index << endl;
+
+        return 0;
+    }
