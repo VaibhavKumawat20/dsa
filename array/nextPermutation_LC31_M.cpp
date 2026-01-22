0e4f0aa90d21de275ff5ae04b82e86676062d40f
VaibhavKumawat20 <vaibhav20kumawat@gmail.com>
2026-01-22 10:24:14 +0530

    add

---
+++ array/nextPermutation_LC31_M.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+void nextPermutation(vector<int>& nums) {
+        int pivot = -1, n = nums.size();
+
+        for(int i=n-2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
+                pivot = i;
+                break;
+            }
+        }
+
+        if(pivot == -1){
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        for(int i=n-1; i>pivot; i--){
+            if(nums[i] > nums[pivot]){
+                swap(nums[i], nums[pivot]);
+                break;
+            }
+        }
+
+        int i = pivot+1, j = n-1;
+        while(i <= j){
+            swap(nums[i++], nums[j--]);
+        }
+    }
+
+int main(){
+    vector<int> nums = {2, 4, 1, 7, 5, 0};
+    nextPermutation(nums);
+    for(int i=0; i<nums.size(); i++){
+        cout << nums[i] << ", ";
+    }
+
+    return 0;
+}
