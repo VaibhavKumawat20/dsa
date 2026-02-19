71f38507864091183216ff5164ff22c8a5081ef9
VaibhavKumawat20 <vaibhav20kumawat@gmail.com>
2026-02-19 17:14:30 +0530

    add

--- Linked List/ImplementationOfLL.cpp
+++ Linked List/ImplementationOfLL.cpp
@@ -34,6 +34,47 @@ class List{
         }
     }
 
+    void push_back(int val){
+        Node* newNode = new Node(val);
+
+        if(head == NULL){
+            head = tail = newNode;
+        }
+        else{
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void pop_front(){
+        if(head == NULL){
+            cout << "LL is empty." << endl;
+            return;
+        }
+
+        Node* temp = head;
+        head = head->next;
+        temp->next = NULL;
+
+        delete temp;
+    }
+
+    void pop_back(){
+        if(head == NULL){
+            cout << "LL is empty." << endl;
+            return;
+        }
+
+        Node* temp = head;
+        while(temp->next != tail){
+            temp = temp->next;
+        }
+        temp->next = NULL;
+
+        delete tail;
+        tail = temp;        
+    }
+
     void printLL(){
         Node* temp = head;
 
@@ -51,6 +92,12 @@ int main(){
     ll.push_front(1);
     ll.push_front(2);
     ll.push_front(3);
+
+    ll.push_back(4);
+
+    ll.pop_front(); 
+
+    ll.pop_back();
 
     ll.printLL();
 
